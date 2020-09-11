3b08de4219a314c3c5dd8df0708a2d9e9c3de306
andrea <andreasilva1801@gmail.com>
2020-09-11 17:43:53 -0300

    modificações exercicio 43

--- src/cadastro.java
+++ src/cadastro.java
@@ -8,23 +8,33 @@ public class cadastro {
 		c2 = new empregado();
 		c3 = new empregado();
 		
-		c1.nome = "ANDREA";
-		c1.cargo = "faxineira";
-		c1.salario = 1200;
+		c1.setNome("ANDREA");
+		c1.setCargo("faxineira");
+		c1.setSalario(1200);
 		
-		c2.nome = "Vitor Cunha";
-		c2.cargo = "vendedor";
-		c2.salario = 3200.12;
+		c2.setNome("Vitor Cunha");
+		c2.setCargo("vendedor");
+		c2.setSalario(3200.12);
 		
-		c3.nome = "ana brito";
-		c3.cargo = "vendedor";
-		c3.salario = 1830.45;
+		c3.setNome ("ana brito");
+		c3.setCargo("vendedor");
+		c3.setSalario(1830.45);
 		
 
 		c1.exibirInformacao();
 		c2.exibirInformacao();
 		c3.exibirInformacao();
-				
+		System.out.println("DEPOIS DO AUMENTO");
+		System.out.println("*******************");
+
+		c1.aumentarSalario(10.0);
+		c2.aumentarSalario(5.0);
+		c3.aumentarSalario(2.0);
+		
+		c1.exibirInformacao();
+		c2.exibirInformacao();
+		c3.exibirInformacao();		
+					
 
 	}
 
